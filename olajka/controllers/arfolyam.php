eb2631075dc78e54665eaae2eead8844f9bb4d07
balto1990 <92270489+balto1990@users.noreply.github.com>
2023-12-02 21:51:29 +0100

    PHP MVC SQL

---
+++ olajka/controllers/arfolyam.php
@@ -0,0 +1,12 @@
+<?php
+
+class Arfolyam_Controller
+{
+	public $baseName = 'arfolyam';  //meghatározzuk, hogy melyik oldalon vagyunk
+	public function main(array $vars) 
+	{
+		$view = new View_Loader($this->baseName."_main");
+	}
+}
+
+?>

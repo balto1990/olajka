a970465298a78cb5433fce6a737dcd0f61b212fb
balto1990 <92270489+balto1990@users.noreply.github.com>
2024-01-25 23:07:14 +0100

    belepes mukodik

--- controllers/belepes.php
+++ controllers/belepes.php
@@ -1,22 +1,10 @@
 <?php
 
-class Belepes_Controller //létrehoztuk a belépés kontroller osztályát
+class Belepes_Controller
 {
-	public $baseName = 'belepes';  //meghatározni, hogy melyik oldalon vagyunk
-	public function main(array $vars) // a router által továbbított paramétereket kapja
+	public $baseName = 'belepes';
+	public function main(array $vars)
 	{
-		$testModel = new Test_Model; //az osztályhoz tartozó modell
-			if(isset($vars['data']))
-			{
-				$retdata = $testModel->get_data($vars['data']); //modellből lekérdezzük a kért adatot		
-				$view = new View_Loader($this->baseName."_main"); //betöltjük a nézetet
-				$view->assign('title', $retdata['title']); //átadjuk a lekérdezett adatokat a nézetnek
-				$view->assign('content', $retdata['content']);
-			}
-		else
-			{
-				echo "No data to show";
-			}	
+		$view = new View_Loader($this->baseName . "_main");
 	}
 }
-?>
--- controllers/beleptet.php
+++ controllers/beleptet.php
@@ -1,24 +1,16 @@
 <?php
 
-class Beleptet_Controller //létrehoztuk a beléptet kontroller osztályát
+class Beleptet_Controller
 {
-	public $baseName = 'beleptet';  //meghatározni, hogy melyik oldalon vagyunk
-	public function main(array $vars) // a router által továbbított paramétereket kapja
+	public $baseName = 'beleptet';
+	public function main(array $vars)
 	{
-		$beleptetModel = new Beleptet_Model;  //A beleptet.php vezérlő a view_loader.php modell előtt még meghívja a beleptet_model.php modellt is, 
-		//ami elvégzi a beléptetést.
-
-		$retData = $beleptetModel->get_data($vars); //modellből lekérdezzük a kért adatokat
+		$beleptetModel = new Beleptet_Model;
+		$retData = $beleptetModel->get_data($vars);
 		if($retData['eredmeny'] == "ERROR")
 			$this->baseName = "belepes";
-		//ha a retData = ERROR, akkor vissza irányít a belepes oldalra (viewloader_paramétere)
-		//csak ezután	
-		//betöltjük a nézetet
 		$view = new View_Loader($this->baseName.'_main');
-		//átadjuk a lekérdezett adatokat a nézetnek
 		foreach($retData as $name => $value)
 			$view->assign($name, $value);
 	}
 }
-
-?>
